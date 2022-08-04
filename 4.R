51fc2afd5f447eaf7b44eb7d192d539981fcc838
brai0 <98900545+brai0@users.noreply.github.com>
2022-08-04 12:57:58 +0530

    Update 4.R

--- 4.R
+++ 4.R
@@ -2,6 +2,7 @@
 df= airquality
 dim(df)
 
+
 sapply(df, class)
 
 which(is.na(df))
@@ -9,10 +10,11 @@ sum(is.na(df))
 x=colSums(is.na(df))
 print(x)
 
+df2=na.omit(df)
+print(df2)
+
 for (i in 1:6){
   df[,i]=ifelse ((is.na(df[,i])), (mean(df[,i],na.rm=TRUE)),(df[,i]))
 }
 print(df)
 
-df2=na.omit(df)
-print(df2)
